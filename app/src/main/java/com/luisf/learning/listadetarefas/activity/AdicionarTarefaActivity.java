dcb7a82b9389ab2699555651932f4a53cbada25e
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-07-05 23:00:22 -0300

    added menu...

--- app/src/main/java/com/luisf/learning/listadetarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/luisf/learning/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package com.luisf.learning.listadetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.luisf.learning.listadetarefas.R;
 
@@ -12,5 +16,21 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_task, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuOption_check_task:
+                Toast.makeText(AdicionarTarefaActivity.this, "Opção Salvar selecionada...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
